7ee500697815589f408dccddb484e8b097de9e54
Ghost-Deba <130713120+Ghost-Deba@users.noreply.github.com>
2025-02-04 23:27:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1225932026293518457')
+    .setApplicationId('1307403158951166014')
     .setType('PLAYING')
     .setURL('https://open.spotify.com/user/nour2282005?si=_lkQtKu2QlC-UVLCkxUL1A') 
     .setState('𓂺𝙵𝚞𝚌𝚔 𝚃𝚑𝚒𝚜 𝙻𝚒𝚏𝚎𓂸')
